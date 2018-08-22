09b0140d28ee567d8045a1ad139330481809a582
WaniFi <skelpd24@gmail.com>
2018-08-22 15:57:50 +0900

    4344 문제 일부 개선

--- baekjoon_4344/main.cpp
+++ baekjoon_4344/main.cpp
@@ -1,7 +1,6 @@
 #include <cstdio>
-double n, m;
-int t, c, stu[1000];
 
+int t, c, n, m, stu[1000];
 int main() {
 	scanf_s("%d", &t);
 	while (t--) {
@@ -13,8 +12,7 @@ int main() {
 		}
 		m /= c;
 		for (int i = 0; i < c; i++) { if (stu[i] > m) n++; }
-		m = (n / c) * 100;
-		printf("%.3lf%%\n", m);
+		printf("%.3lf%%\n", (double)n/c*100);
 	}
 	return 0;
 }
